9e11059b093a5a7630de40867f1c40ea3994f4c0
shaif-ali-javad <shaifalijavad60@gmail.com>
2025-04-22 19:06:09 +0530

    Updates

---
+++ assets/services-CY9pG5rw.js
@@ -0,0 +1,1 @@
+import{r,j as s,M as e,F as o}from"./index-DWy7FkW0.js";import{IThomePage as t}from"./it-amc-D5AvJstV.js";import"./ChevronUpIcon-Di54VSHU.js";class n extends r.Component{render(){return s.jsxs("div",{children:[s.jsx(e,{}),s.jsx(t,{}),s.jsx(o,{})]})}}export{n as default,n as services};
