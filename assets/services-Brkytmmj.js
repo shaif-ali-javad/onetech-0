60c26a51cb0d8382f952ad41b4f1a1be99d1a747
shaif-ali-javad <shaifalijavad60@gmail.com>
2025-04-24 18:55:54 +0530

    Updates

---
+++ assets/services-Brkytmmj.js
@@ -0,0 +1,1 @@
+import{r,j as s,M as e,F as o}from"./index-DyONCKSj.js";import{IThomePage as t}from"./it-amc-ayZwDGaS.js";import"./ChevronUpIcon-B1gihgkf.js";class n extends r.Component{render(){return s.jsxs("div",{children:[s.jsx(e,{}),s.jsx(t,{}),s.jsx(o,{})]})}}export{n as default,n as services};
